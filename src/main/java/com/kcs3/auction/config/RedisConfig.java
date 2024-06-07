f99ae1958bfc3f25fb3a841171b2b940663e3e22
leeinhong95 <leeinhong9512@gmail.com>
2024-06-08 02:03:55 +0900

    feat:  redis 클러스터 연동

--- src/main/java/com/kcs3/auction/config/RedisConfig.java
+++ src/main/java/com/kcs3/auction/config/RedisConfig.java
@@ -5,6 +5,7 @@ import com.kcs3.auction.dto.HotItemsDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -12,35 +13,31 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private String redisPort;
-
-
+    @Value("${spring.redis.cluster.nodes}")
+    private String redisClusterNodes;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(redisHost);
-        redisStandaloneConfiguration.setPort(Integer.parseInt(redisPort));
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
-        return lettuceConnectionFactory;
+        List<String> nodes = Collections.singletonList(redisClusterNodes);
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(nodes);
+        return new LettuceConnectionFactory(clusterConfiguration);
     }
 
     @Bean
-    public RedisTemplate<?,?> redisTemplate() {
+    public RedisTemplate<String, HotItemsDto> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, HotItemsDto> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
     }
-
 }
