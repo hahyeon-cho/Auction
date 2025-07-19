af8f8c2bea1a1e0e17d892189ca89187b882dd2e
hahyeon-cho <mondlicht3724@gmail.com>
2025-07-19 22:48:26 +0900

    refactor: ItemSearchDocument를 클래스 기반 구조로 변경
    - recode -> class
    - Elasticsearch는 검색 결과를 JSON 형태로 반환하므로,
      안정적인 역직렬화와 확장성을 위해 변경

--- src/main/java/com/kcs3/auction/document/ItemSearchDocument.java
+++ src/main/java/com/kcs3/auction/document/ItemSearchDocument.java
@@ -1,15 +1,27 @@
 package com.kcs3.auction.document;
 
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Document(indexName = "items")
-@Builder
-public record ItemSearchDocument(
-    @Id Long itemId,
-    String itemTitle,
-    Long createdAt
-) {
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ItemSearchDocument {
 
+    @Id
+    private Long itemId;
+    private String itemTitle;
+    private LocalDateTime createdAt;
+
+    @Builder
+    public ItemSearchDocument(Long itemId, String itemTitle, LocalDateTime createdAt) {
+        this.itemId = itemId;
+        this.itemTitle = itemTitle;
+        this.createdAt = createdAt;
+    }
 }
