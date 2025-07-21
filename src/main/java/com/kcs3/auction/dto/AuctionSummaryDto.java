d20ba71844e417b9813d8217f1ff9021b80d858b
hahyeon-cho <mondlicht3724@gmail.com>
2025-07-21 15:51:58 +0900

    refactor: AuctionSummaryDto 생성자 방식 적용
    - @Builder 제거 및 생성자 방식 적용

--- src/main/java/com/kcs3/auction/dto/AuctionSummaryDto.java
+++ src/main/java/com/kcs3/auction/dto/AuctionSummaryDto.java
@@ -3,9 +3,7 @@ package com.kcs3.auction.dto;
 import com.kcs3.auction.entity.AuctionCompleteItem;
 import com.kcs3.auction.entity.AuctionProgressItem;
 import java.time.LocalDateTime;
-import lombok.Builder;
 
-@Builder
 public record AuctionSummaryDto(
     String itemTitle,
     String location,
@@ -15,24 +13,24 @@ public record AuctionSummaryDto(
     LocalDateTime bidFinishTime
 ) {
     public static AuctionSummaryDto from(AuctionProgressItem item) {
-        return AuctionSummaryDto.builder()
-            .itemTitle(item.getItemTitle())
-            .location(item.getLocation())
-            .startPrice(item.getStartPrice())
-            .buyNowPrice(item.getBuyNowPrice())
-            .maxPrice(item.getMaxPrice())
-            .bidFinishTime(item.getBidFinishTime())
-            .build();
+        return new AuctionSummaryDto(
+            item.getItemTitle(),
+            item.getLocation(),
+            item.getStartPrice(),
+            item.getBuyNowPrice(),
+            item.getMaxPrice(),
+            item.getBidFinishTime()
+        );
     }
 
     public static AuctionSummaryDto from(AuctionCompleteItem item) {
-        return AuctionSummaryDto.builder()
-            .itemTitle(item.getItemTitle())
-            .location(item.getLocation())
-            .startPrice(item.getStartPrice())
-            .buyNowPrice(item.getBuyNowPrice())
-            .maxPrice(item.getMaxPrice())
-            .bidFinishTime(item.getBidFinishTime())
-            .build();
+        return new AuctionSummaryDto(
+            item.getItemTitle(),
+            item.getLocation(),
+            item.getStartPrice(),
+            item.getBuyNowPrice(),
+            item.getMaxPrice(),
+            item.getBidFinishTime()
+        );
     }
 }
